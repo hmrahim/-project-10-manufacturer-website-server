cb16f16dd1285a8e7dd274b789d856058fdc4d84
hmrahim <h.m.rahimnet@gmail.com>
2022-05-22 16:21:18 +0600

    verify admin admin and verify jwt token create done

--- index.js
+++ index.js
@@ -2,12 +2,58 @@ const express = require("express")
 const cors = require("cors")
 const { application } = require("express")
 require("dotenv").config()
+const jwt = require("jsonwebtoken")
 const port = process.env.PORT || 5000
 const app = express()
 
 
 app.use(cors())
 app.use(express.json())
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.trq2z.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const run = async()=> {
+    try {
+       await client.connect()
+       console.log("Database connected");
+
+    //    COLLECTIONS.................................
+    const userCollection = client.db("falcon-electronics").collection("users")
+
+    // all routes,,,,,,,,,,,,,,,,,,,,,,,,,
+    
+
+    app.get("/users",verify,async(req,res)=> {
+        const data = await userCollection.find().toArray()
+        res.send(data)
+    })
+    
+
+
+
+
+
+    
+
+    const verifyAdmin = async(req,res,next)=> {
+        const requester = req.decoded.email
+        const requesterData = await userCollection.findOne({email:requester})
+        if(requesterData.role === "admin"){
+        next()
+        }else{
+          res.status(403).send("unAuthorize access")
+        }
+  
+      }
+        
+    } finally{
+
+    }
+
+}
+run().catch(console.dir)
 
 app.get("/",(req,res)=> {
     res.send("hello from home")
@@ -17,3 +63,28 @@ app.get("/",(req,res)=> {
 app.listen(port,()=> {
     console.log("server is running on port no 5000");
 })
+
+
+
+function verify(req,res,next){
+    const header = req.headers.authorization
+   
+
+    if(!header){
+       return res.send({message:"unauthorize access"})
+    }
+    const token = header.split(" ")[1]
+    jwt.verify(token, process.env.JWT_SECRET, function(err, decoded) {
+        if(err){
+            return res.status(403).send({message:"forbidded access"})
+        }
+        req.decoded = decoded
+        next()
+      });
+    
+   
+     
+   
+   }
+
+
